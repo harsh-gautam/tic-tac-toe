9f71d18d750f12d2d61edd9a500ba9007c78c61f
Harsh Gautam <hgautam208@gmail.com>
2021-09-13 23:38:37 +0530

    update player info on UI on load and update current player

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,8 @@ const displayController = (function (document) {
   const _startScreen = document.querySelector(".start-menu");
   const _gameScreen = document.querySelector(".game-wrapper");
   const _playerTwo = document.querySelector("#player-two");
+  const _p1Name = document.querySelector(".p1-name");
+  const _p2Name = document.querySelector(".p2-name");
 
   const toggleDisplay = (type) => {
     if (type == "form") {
@@ -11,6 +13,26 @@ const displayController = (function (document) {
     } else if (type === "newgame" || type === "exit") {
       _startScreen.style.display = "flex";
       _gameScreen.style.display = "none";
+    }
+  };
+
+  const _updatePlayerNames = (p1Name, p2Name) => {
+    _p1Name.innerHTML = "";
+    _p2Name.innerHTML = "";
+    _p1Name.insertAdjacentHTML(
+      "afterbegin",
+      `<i class="bi bi-person-fill"></i>${p1Name}`
+    );
+    if (p2Name === "AI") {
+      _p2Name.insertAdjacentHTML(
+        "afterbegin",
+        `<i class="bi bi-cpu-fill"></i>${p2Name}`
+      );
+    } else {
+      _p2Name.insertAdjacentHTML(
+        "afterbegin",
+        `<i class="bi bi-person-fill"></i>${p2Name}`
+      );
     }
   };
 
@@ -35,12 +57,17 @@ const displayController = (function (document) {
     });
   };
 
-  const setupDOM = () => {
+  const _setupDOM = () => {
     _attachPvC();
     _attachPvP();
   };
 
-  return { setupEventListener, toggleDisplay, setupDOM };
+  return {
+    setupEventListener,
+    toggleDisplay,
+    setupDOM: _setupDOM,
+    updatePlayerNames: _updatePlayerNames,
+  };
 })(document);
 
 const gameBoard = (function () {
@@ -48,6 +75,7 @@ const gameBoard = (function () {
   let _difficulty;
   let _p1;
   let _p2;
+  let _currentPlayer = null;
   let _board = [[], [], []];
 
   const _setParameters = (p1, p2, mode, difficulty) => {
@@ -66,15 +94,30 @@ const gameBoard = (function () {
     };
   };
 
+  const _playGame = () => {
+    if (_currentPlayer === null) _currentPlayer = p1;
+  };
+
+  const _updateCurrentPlayer = () => {
+    if (_currentPlayer === _p1) _currentPlayer = _p2;
+    else _currentPlayer = _p1;
+  };
+
+  const _getCurrentPlayer = () => _currentPlayer;
+
   return {
     setParameters: _setParameters,
     getParameters: _getParameters,
+    playGame: _playGame,
+    getCurrentPlayer: _getCurrentPlayer,
+    updateCurrentPlayer: _updateCurrentPlayer,
   };
 })();
 
-const Player = (pname, marker) => {
+const Player = (identity, pname, marker) => {
   const getName = () => pname;
   const getMarker = () => marker;
+  const getIdentity = () => identity;
   const changeMarker = (newMarker = "x") => {
     marker = newMarker;
   };
@@ -94,18 +137,30 @@ const handleForm = (e) => {
   const difficulty = e.target.difficulty.value;
 
   // create players
-  p1 = Player(p1Name, "x");
-  p2 = Player(p2Name, "o");
+  p1 = Player("p1", p1Name, "x");
+  p2 = Player("p2", p2Name, "o");
 
   // create game state
   gameBoard.setParameters(p1, p2, gameMode, difficulty);
-  console.log(gameBoard.getParameters());
-  // displayController.toggleDisplay("form");
+  game = gameBoard.getParameters();
+
+  // display the board
+  displayController.toggleDisplay("form");
+  startGame();
 };
 
 const handleNewGame = () => {
   displayController.toggleDisplay("newgame");
 };
+
+function startGame() {
+  displayController.updatePlayerNames(game.p1.getName(), game.p2.getName());
+  // displayController.updateCurrentPlayer(
+  //   gameBoard.getCurrentPlayer().getMarker()
+  // );
+  gameBoard.playGame();
+  console.log(gameBoard.getCurrentPlayer());
+}
 
 displayController.setupDOM();
 displayController.setupEventListener(form, "submit", handleForm);
